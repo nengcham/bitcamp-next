fde081f3011486cdd165a3266045d339dd7f3690
nengcham <nengcham7@gmail.com>
2022-04-08 18:12:46 +0900

    useEffect

--- pages/user/user-list.js
+++ pages/user/user-list.js
@@ -1,6 +1,8 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styles from '../common/style/table.module.css'
 
-const Table = ({ columns, colspan, data}) => {
+const Table = ({columns, colspan, data}) => {
     return(
         <table className={styles.table}>
             <thead>
@@ -11,10 +13,17 @@ const Table = ({ columns, colspan, data}) => {
                 </tr>
             </thead>
             <tbody>
-                <tr className={styles.tr}>
-                    { data.length == 0 ?<td colSpan={colspan} className={styles.td}>데이터가 없습니다.</td>
-                    :<td colSpan={colspan} className={styles.td}>데이터가 있습니다.</td>}
-                </tr>
+                { data.length == 0 ? <tr className={styles.tr}>
+                                     <td colSpan={colspan} className={styles.td}>데이터가 없습니다.</td>
+                                     </tr>
+                :data.map((user) => (
+                    <tr className={styles.tr} key={user.username}>
+                        <td className={styles.td}>{user.username}</td>
+                        <td className={styles.td}>{user.password}</td>
+                        <td className={styles.td}>{user.name}</td>
+                        <td className={styles.td}>{user.telephone}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
@@ -22,8 +31,13 @@ const Table = ({ columns, colspan, data}) => {
 
 export default function Userlist(){
     const columns = ["Username", "Password", "Name", "Telephone"]
-    const data = []
+    const [data, setData] = useState([])
     const count = data.length
+    useEffect(()=>{
+        axios.get('http://localhost:5000/api/user/list')
+        .then(res=>{setData(res.data.users)})
+        .catch(err=>{})
+    },[])
     return(<>
         <h1>사용자 목록</h1>
         {count != 0 && <h3>회원수 : { count }명</h3>}
